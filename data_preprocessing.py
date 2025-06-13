1e929b3d11b45bf65acc922805cb02e8df1fd756
binxni <126181703+binxni@users.noreply.github.com>
2025-06-13 17:15:03 +0900

    Create data_preprocessing.py

---
+++ data_preprocessing.py
@@ -0,0 +1,24 @@
+def id_class_name(class_id, classes):
+    for key, value in classes.items():
+        if class_id == key:
+            return value
+
+def img_preprocess(image):
+    height, _, _ = image.shape
+    image = image[int(height / 2):, :, :]
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+    image = cv2.resize(image, (200, 66))
+    image = cv2.GaussianBlur(image, (5,5), 0)
+    image = image/255
+    return image
+
+camera = mycamera.MyPiCamera(640, 480)
+
+_, image = camera.read()
+image = cv2.flip(image, -1)
+imagednn = image
+image_ok = 0
+image_find_ok = 0
+
+box_size =0
+carState="stop"
